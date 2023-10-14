706d7053de92f93fe142cf3bdd7caf279481f3d9
sabrina.rashid <sabrina.rashid@monstar-lab.com>
2023-10-14 12:25:34 +0600

    modifying cart and login files

--- test/features/step-definations/Cart.ts
+++ test/features/step-definations/Cart.ts
@@ -1,43 +1,34 @@
 import { Given, When } from "@wdio/cucumber-framework";
 import LoginPage from "../../pageobjects/login.page";
+//import locatorPage from "../../pageobjects/locator.page";
+import cartPage from "../../pageobjects/cart.page";
 
 Given(/^Launch the browser$/, async function () {
-	await browser.url("http://automationexercise.com");
+	//await browser.url("/");
+	cartPage.open()
 	await browser.maximizeWindow();
 	await browser.pause(3000);
 
 });
 
 When(/^click on add to the cart$/, async function () {
-	// let scrollEle= await $(`//h2[contains(text(),'Brands')]`)
-	let scrollEle = await $(`//a[normalize-space()='Women']`);
-	await scrollEle.scrollIntoView();
+	await browser.scroll(0, 700);
+	(await cartPage.addToCart).click();
 	await browser.pause(3000);
-	let ele = await $(`a[href='/product_details/1']`);
-	//ele.isDisplayed()
-	ele.waitForDisplayed();
-	//await browser.pause(3000);
-	let ele2 = $(
-		`//body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/a[1]`
-	);
-	ele2.click();
-	await browser.pause(3000);
-	//await browser.debug(5000)
+
+
 });
 
 When(/^click on view cart$/, async function () {
-	let viewCart = await $(`//u[contains(text(),'View Cart')]`);
-	await viewCart.click();
-	await browser.pause(3000);
-	let checkout = await $(`//a[normalize-space()='Proceed To Checkout']`);
-	await checkout.click();
-	await browser.pause(4000);
+	(await cartPage.viewCart).click();
+	await browser.pause(2000);
+	(await cartPage.checkout).click();
 
 });
 
 When(/^click on login link and enter login credentials$/, async function () {
-	let loginLink = await $(`//u[contains(text(),'Register / Login')]`);
-	await loginLink.click();
+	await cartPage.loginLink.click();
+	await browser.pause(2000);
 	//login-method
 	await LoginPage.login(process.env.TEST_STD_USERNAME!, process.env.TEST_PASSWORD!)
 	await browser.pause(4000);
@@ -45,14 +36,10 @@ When(/^click on login link and enter login credentials$/, async function () {
 
 
 // When(/^click on subscription$/, async function () {
-// 	let subscription = await $(`//h2[normalize-space()='Subscription']`)
-// 	subscription.scrollIntoView()
+// 	await browser.scroll(0, 700);
 
 // 	//subscription email
-// 	let subscriptionEmail = await $(`#susbscribe_email`);
-// 	await subscriptionEmail.setValue("sabrinamdel@gmail.com");
-// 	await browser.pause(2000);
-// 	let subscriptionButton = await $(`.fa.fa-arrow-circle-o-right`);
-// 	subscriptionButton.click();
+// 	await (await locatorPage.subscriptionEmail).setValue(process.env.TEST_EMAIL!);
+// 	(await locatorPage.subscriptionButton).click()
 // 	await browser.pause(4000);
 // });
