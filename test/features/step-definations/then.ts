32b99dfc12186066d17e3b3fd74af2c358236e5e
sabrina.rashid <sabrina.rashid@monstar-lab.com>
2023-10-26 12:21:35 +0600

    modifying then file

--- test/features/step-definations/then.ts
+++ test/features/step-definations/then.ts
@@ -12,22 +12,22 @@ Then(/^Verify the (.*)$/, async function (Header) {
     expect(await registrationPage.headerText).toHaveText(Header);;
 });
 
-Then(/^Click on DOB (.*) and (.*) and (.*)$/, async function (date:number,month:string,year:string) {
-    registrationPage.enterDOB(date,month,year)
-   await browser.pause(5000);
+Then(/^Click on DOB (.*) and (.*) and (.*)$/, async function (date: number, month: string, year: string) {
+    registrationPage.enterDOB(date, month, year)
+    await browser.pause(5000);
 });
 
 
 Then(/^User will select newsletter and special offer$/, async function () {
     registrationPage.clickNewsletter()
-   await browser.pause(5000);
-    	
+    await browser.pause(5000);
+
 });
 
 Then(/^User will enter the following details$/, async function () {
     registrationPage.createAccount(faker.person.firstName(), faker.person.lastName(), resources.companyName, faker.location.streetAddress());
     registrationPage.otherInformation(resources.countryName, faker.location.city(), faker.location.state(), faker.location.zipCode(), faker.phone.number());
-    await browser.pause(6000); 
- 
+    await browser.pause(6000);
+
 });
 
